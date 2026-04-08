90ec7076384b093c5f47de3cfdb6033f25f060d6
Mihai Anton <antonmihai58@gmail.com>
2026-04-08 11:21:19 +0300

    Fixes widget url

--- src/components/DatacardsWidget.tsx
+++ src/components/DatacardsWidget.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 
 const WIDGET_SRC =
-  "http://localhost:3000/a/mihai/personal/intercom?theme=light";
-// http://localhost:3000/mihai/personal
+  "https://app.datacards.ai/a/aiflow/company-questions/intercom?theme=light";
+
 /** Fixed Datacards embed + resize handshake; mounted once in App for all routes. */
 export function DatacardsWidget() {
   useEffect(() => {
